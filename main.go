b1bde5ac41a79f5101dd84dd434be8d942db3609
jackyspy <jackyspy@hotmail.com>
2016-11-09 16:25:28 +0800

    remove apps.txt support

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/elazarl/goproxy"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
 	"strings"
 )
 
@@ -52,27 +49,6 @@ func NewProxy() *goproxy.ProxyHttpServer {
 			return r, goproxy.NewResponse(r, "text/html;charset=utf-8", http.StatusOK, string(data))
 		})
 
-	// proxy.OnRequest(goproxy.ReqHostIs("PROXY")).DoFunc(
-	// 	func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-	// 		filename := path.Join(".", ctx.Req.URL.Path)
-	// 		fmt.Println(filename)
-	// 		if strings.Contains(filename, "..") {
-	// 			return r, notFound(r)
-	// 		}
-
-	// 		if _, err := os.Stat(filename); os.IsNotExist(err) {
-	// 			return r, notFound(r)
-	// 		}
-
-	// 		bytes, err := ioutil.ReadFile(filename)
-	// 		if err != nil {
-	// 			ctx.Warnf("%s", err)
-	// 			return r, notFound(r)
-	// 		}
-
-	// 		return r, goproxy.NewResponse(r, "application/octet-stream", http.StatusOK, string(bytes))
-	// 	})
-
 	return proxy
 }
 
@@ -92,34 +68,26 @@ func newAppUrlNoUpdate() AppUrl {
 	return AppUrl{
 		IsUpdate: false,
 		PkgName:  "com.keylab.speech.core.yueme",
-		Url:      "",
 		Version:  "3.02.008",
 	}
 }
 
-func notFound(r *http.Request) *http.Response {
-	return goproxy.NewResponse(r, "text/plain", http.StatusNotFound, "Not Found")
-}
-
 func getAppUrls(count int) []AppUrl {
-	var result []AppUrl
-	c := 0
+	result := []AppUrl{}
 
-	for _, x := range getAppUrlsFromApks() {
-		result = append(result, x)
-		if c++; c == count {
-			return result
+	files, err := ioutil.ReadDir(".")
+	if err == nil {
+		for _, file := range files {
+			filename := strings.ToLower(file.Name())
+			if file.IsDir() || !strings.HasSuffix(filename, ".apk") {
+				continue
+			}
+
+			result = append(result, newAppUrl(fmt.Sprintf("http://PROXY/%s", quote(filename))))
 		}
 	}
 
-	for _, x := range getAppUrlsFromFile() {
-		result = append(result, x)
-		if c++; c == count {
-			return result
-		}
-	}
-
-	for ; c < count; c++ {
+	for c := len(result); c < count; c++ {
 		result = append(result, newAppUrlNoUpdate())
 	}
 
@@ -128,55 +96,6 @@ func getAppUrls(count int) []AppUrl {
 
 func quote(s string) string {
 	return (&url.URL{Path: s}).RequestURI()
-}
-
-func getAppUrlsFromApks() []AppUrl {
-	var result []AppUrl
-
-	files, err := ioutil.ReadDir(".")
-	if err != nil {
-		return result
-	}
-
-	for _, file := range files {
-		filename := strings.ToLower(file.Name())
-		if file.IsDir() || !strings.HasSuffix(filename, ".apk") {
-			continue
-		}
-
-		result = append(result, newAppUrl(fmt.Sprintf("http://PROXY/%s", quote(filename))))
-	}
-
-	return result
-}
-
-func getAppUrlsFromFile() []AppUrl {
-	var result []AppUrl
-
-	f, err := os.Open("apps.txt")
-	if err != nil {
-		return result
-	}
-	defer f.Close()
-
-	rd := bufio.NewReader(f)
-	for {
-		line, err := rd.ReadString('\n')
-		if err != nil && io.EOF != err {
-			break
-		}
-
-		line = strings.TrimSpace(line)
-		if line != "" && !strings.HasPrefix(line, "#") {
-			result = append(result, newAppUrl(line))
-		}
-
-		if io.EOF == err {
-			break
-		}
-	}
-
-	return result
 }
 
 type muxHandler struct {
@@ -202,4 +121,3 @@ func main() {
 	mux := muxHandler{proxyHandler: proxy, defaultHandler: http.FileServer(http.Dir("./"))}
 	log.Fatal(http.ListenAndServe(*addr, &mux))
 }
-
